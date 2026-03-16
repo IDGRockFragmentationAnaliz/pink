f1828477c6e66c34df5b80cb3c9086cffe6b2b81
koladik <koladik@gmail.com>
2026-03-16 14:54:13 +0300

    update main

--- python/main.py
+++ python/main.py
@@ -19,30 +19,79 @@ from pathlib import Path
 from matplotlib import pyplot as plt
 from pink import numpy_conv
 import numpy as np
+import cv2
 
+from skeletonize.matcher import Matcher
 
 def main():
     # image_path = Path("./test/cercle11.pgm")
     # image_path = Path("./test/rectangle_rips.pgm")
-    image_path = Path("test/gel.pgm")
+    image_path = Path("C:/Users/Koladik/Desktop/Skeleton/pink2/images/test_2.pgm")
     # image_path = Path("test/square_hole.pgm")
 
-
-    print(str(image_path))
     image = pink.cpp.readimage(str(image_path))
-    print(image)
-    #exit()
-    # image_numpy = numpy_conv.pink2numpy(image)
-    # #image = pink.cpp.dilatball_test(image, 1)
-    # plt.imshow(image_numpy)
-    # plt.show()
+    image_numpy_start = numpy_conv.pink2numpy(image).copy()
 
-    image2 = pink.cpp.lhthinpar_test(image, 10000)
+    image = pink.cpp.lhthinpar_test(image, 10000)
+    image = pink.cpp.lhthinpar_asymmetric_test(image, -1)
+    image = pink.cpp.crestrestoration(image, -1, 4)
 
-    image_numpy = numpy_conv.pink2numpy(image2)
-    print(image_numpy)
+    #image_numpy_skel1 = numpy_conv.pink2numpy(image).copy()
 
-    plt.imshow(image_numpy)
+    # image = pink.cpp.lambdaskel(image, 4, 100)
+    image_numpy_skel1 = numpy_conv.pink2numpy(image).copy()
+
+    #image = pink.cpp.segmentlines(image, 4, 100,255)
+
+    image_numpy_skel2 = numpy_conv.pink2numpy(image).copy()
+    image_numpy = image_numpy_skel2.copy()
+    wells = Matcher(image_numpy)
+    wells.run()
+
+
+    fig = plt.figure(figsize=(12, 6))
+
+    ax1 = fig.add_subplot(1, 3, 1)
+    ax1.imshow(image_numpy_start)
+    ax1.set_title('Было')
+    ax1.axis('off')
+
+    ax2 = fig.add_subplot(1, 3, 2)
+    ax2.imshow(image_numpy_skel1)
+    ax2.set_title('Стало')
+    ax2.axis('off')
+    ax2.sharex(ax1)
+    ax2.sharey(ax1)
+
+    ax3 = fig.add_subplot(1, 3, 3)
+    ax3.imshow(image_numpy)
+    ax3.set_title('Стало')
+    ax3.axis('off')
+    ax3.sharex(ax1)
+    ax3.sharey(ax1)
+
+    # ax4 = fig.add_subplot(2, 3, 4)
+    # ax4.imshow(image_numpy_skel3)
+    # ax4.set_title('Стало')
+    # ax4.axis('off')
+    # ax4.sharex(ax1)
+    # ax4.sharey(ax1)
+
+    # ax5 = fig.add_subplot(2, 3, 5)
+    # ax5.imshow(image_numpy_skel4)
+    # ax5.set_title('Стало')
+    # ax5.axis('off')
+    # ax5.sharex(ax1)
+    # ax5.sharey(ax1)
+
+    # ax6 = fig.add_subplot(2, 3, 6)
+    # ax6.imshow(image_numpy_skel4)
+    # ax6.set_title('Стало')
+    # ax6.axis('off')
+    # ax6.sharex(ax1)
+    # ax6.sharey(ax1)
+
+    plt.tight_layout()
     plt.show()
 
 
